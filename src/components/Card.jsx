8df65b9795b3f6954f6ba6d0afe84b28543667a5
selman <gunduzgece546@gmail.com>
2022-07-02 19:30:39 +0300

    button

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -18,39 +18,49 @@ const Card = ({data,getUser}) => {
   } = data
   console.log(data)
   return (
-    <div className="card-container">
-      <div className="card">
-        <div className="user-name user-info">
-          <div className="img " style={{ background: `url(${picture.large}) center center no-repeat `,width:"120px", height:"120px",backgroundSize: "contain",borderRadius:"50%"  }}>
-            
+    <>
+   
+   <div className="card" >
+    <div className="left">
+      <div className="img" style={{background : ` url(${picture.large})`}}>
+        
 
-          </div>
-      <h5 className="name">{name.title} {name.first} {name.last}</h5>
-        </div>
-        <div className="user-name icon user-info">
-  <img src={emailSvg} alt="email" />  <p className="email">eric@hotmail.com</p>
-        </div>
-        <div className="user-name icon user-info">
-           <img src={locaSvg} alt="" /> <p className="tel">05445878878</p>
-          </div>
-          <div className="user-name icon user-info">
-          <img src={phoneSvg} alt="" /> <p className="loca">Ankara-TURKEY</p>
-          </div>
-          <div className="user-name user-info-contact">
-           <p className="age">AGE: 34</p>
-          </div>
-          <div className="user-name user-info-contact">
-          <p className="register">Register Date: 2004-07-12</p>
-
-
-          </div>
-       
-            </div>
-            <div className="btn">
-      <button onClick={() => getUser()}>Random User</button>
-
-            </div>
+      </div>
+      <div className="icon-box">
+      <img src={emailSvg} alt="email" className=" icon" />
+     
+   
+      <img src={phoneSvg} alt="location" className=" icon" />
+   
+    
+      <img src={locaSvg} alt="phone" className=" icon" />
     </div>
+  
+    </div>
+    <div className="right">
+      <p className="name"> {name.title} {name.first} {name.last} </p>
+      <p className="email" >{email} </p>
+      <p className="phone">
+        {cell}
+      </p>
+      <p className="loca">
+        {location.city}-{location.country} 
+      </p>
+      <div className="age" >
+
+     <p>
+        Age : {dob.age}
+      </p>
+      <p className="register">
+        Register Date : {registered.date.slice(0,10)}
+      </p>
+      </div>
+    </div>
+    
+   </div> 
+    <button onClick={() => getUser()}>Random User</button>
+    </>
+     
   )
 }
 
